7ddbd6d1ef52ec2d8bf7f9bcaed332453b88ffeb
satya-prakashan <satyaprakash6945@gmail.com>
2022-11-03 12:34:38 +0530

    v1

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
